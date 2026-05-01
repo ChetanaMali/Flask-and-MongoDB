ada097b2c664fd865b8a1a74193761b584cbc980
ChetanaMali <chetanamali9763@gmail.com>
2026-05-01 15:39:09 +0530

    add requirement.txt file to install the package, add .env file, create mongodb database , then collection of that database and add our login data in that collection

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 from flask import Flask, request, render_template
 from datetime import date
+# load mongodb pachage
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+import pymongo
+
+#load env file
+from dotenv import load_dotenv
+load_dotenv() #load the env file
+
+MONGO_URL = os.getenv('MONGO_URL') # assign the URL to a variable
+client = pymongo.MongoClient(MONGO_URL)  # create a MongoClient object using the URL
+
+
+
+db = client.test_database # create or access a database named 'test_database'
+collection = db['chetana_collection'] # create or access a collection named 'chetana_collection' within the 'test_database'
 
 app = Flask(__name__)
 
@@ -9,7 +26,8 @@ def home():
 
 @app.route('/login', methods = ['POST'])
 def login_page():
-    name = request.form
-    return name
+    login_data = dict(request.form)
+    collection.insert_one(login_data) # insert the login data into the MongoDB collection
+    return 'data posted successfully'
 if __name__ == '__main__':
     app.run(port = 8080, debug=True)
