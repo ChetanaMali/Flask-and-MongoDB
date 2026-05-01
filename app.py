60fd502211aefeef10a0eb4606cb29500151bfee
ChetanaMali <chetanamali9763@gmail.com>
2026-05-01 10:20:05 +0530

    import request from Flask and give value is URL and get result accordingly

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'This is a simple Flask application:)'
 
-@app.route('/api/<name>')
-def api(name):
-    result = 'hello this is ' + name
-    return result
+@app.route('/add/<a>/<b>')
+def add(a,b):
+    result = int(a) + int(b)
+    return str(result)
+
+@app.route('/api')
+def api():
+    name = request.values.get('name')
+    return f'Hello, {name}! This is a simple API endpoint.'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port = 8080, debug=True)
